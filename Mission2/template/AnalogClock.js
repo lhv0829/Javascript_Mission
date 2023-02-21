b4ee58feea27b2f2ede3abedcfb421aed35d28e9
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2023-02-22 04:48:45 +0900

    시침,분침,초침 움직이기까지 초침이 완벽하게 안맞는다 왜지..? Date받아와서 현재 시간 보여주는 것 부터

--- Mission2/template/AnalogClock.js
+++ Mission2/template/AnalogClock.js
@@ -30,6 +30,30 @@ const AnalogClock = $container => {
     });
     $container.appendChild(timeDOM[i]);
   }
+  // let deg = hour.style.getPropertyValue(--deg);
+  // console.log('나오나?deg가?' + deg);
+  let deg_hour = getComputedStyle(hour).getPropertyValue('--deg');
+  let deg_minute = getComputedStyle(minute).getPropertyValue('--deg');
+  let deg_second = getComputedStyle(second).getPropertyValue('--deg');
+  console.log(`hour는? ${deg_hour}`);
+  console.log(`minute는? ${deg_minute}`);
+  console.log(`second는? ${deg_second}`);
+  deg_second = setInterval(() => {
+    deg_second += 6;
+    //hour.setProperty('--deg', deg_hour);
+    second.style.setProperty('--deg', deg_second);
+  }, 1000);
+  deg_minute = setInterval(() => {
+    deg_minute += 0.1;
+    //hour.setProperty('--deg', deg_hour);
+    minute.style.setProperty('--deg', deg_minute);
+  }, 1000);
+  deg_hour = setInterval(() => {
+    deg_hour += 0.5/60;
+    //hour.setProperty('--deg', deg_hour);
+    hour.style.setProperty('--deg', deg_hour);
+  }, 1000);
+  
 
 };
 
