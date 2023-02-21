b1de61f257ec886dcfe7772ce4d7132249eae8e1
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2023-02-21 18:29:48 +0900

    오..mission1 끝!

--- Mission1/template/app.js
+++ Mission1/template/app.js
@@ -1,30 +1,17 @@
 // do something!
 const toggle = document.getElementsByClassName('toggle')[0];
 const sidebar = document.getElementsByTagName('nav')[0];
-// let isOpen = false;
 
-document.addEventListener("DOMContentLoaded", () => {
+let open = localStorage.getItem('open');
+if(open === 'true') sidebar.classList.add('active');
+
+window.addEventListener("load", () => {
   document.body.style.visibility = 'visible';
-  let open = localStorage.getItem('open');
-  console.log(`처음 open은? ${open}`);
-  if(open === 'true'){
-    sidebar.classList.add('active');
-    console.log("okay");
-  }
+  document.body.classList.remove('preload');
 });
-document.body.classList.remove('preload');
 
 toggle.addEventListener("click", () => {
   sidebar.classList.toggle('active');
-  console.log(sidebar.classList.contains('active'));
-  if(sidebar.classList.contains('active')){
-    // isOpen = true;
-    localStorage.setItem('open', true);
-    // console.log(`open 여부 : ${isOpen}`);
-  }
-  if(!sidebar.classList.contains('active')){
-    // isOpen = false;
-    localStorage.setItem('open', false)
-  } 
-  console.log(`변경된 open은? ${localStorage.getItem('open')}`);
+  if(sidebar.classList.contains('active')) localStorage.setItem('open', true);
+  if(!sidebar.classList.contains('active')) localStorage.setItem('open', false);
 });
