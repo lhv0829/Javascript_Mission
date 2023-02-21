dfd11be022e024069ce587e2c1b1e3e2c48e7fa3
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2023-02-22 03:52:41 +0900

    뭐가변경된거..?

--- Mission1/template/app.js
+++ Mission1/template/app.js
@@ -1,17 +1,17 @@
 // do something!
-const toggle = document.getElementsByClassName('toggle')[0];
-const sidebar = document.getElementsByTagName('nav')[0];
+const toggle = document.getElementsByClassName("toggle")[0];
+const sidebar = document.getElementsByTagName("nav")[0];
 
-let open = localStorage.getItem('open');
-if(open === 'true') sidebar.classList.add('active');
+let open = localStorage.getItem("open");
+if (open === "true") sidebar.classList.add("active");
 
 window.addEventListener("load", () => {
-  document.body.style.visibility = 'visible';
-  document.body.classList.remove('preload');
+  document.body.style.visibility = "visible";
+  document.body.classList.remove("preload");
 });
 
 toggle.addEventListener("click", () => {
-  sidebar.classList.toggle('active');
-  if(sidebar.classList.contains('active')) localStorage.setItem('open', true);
-  if(!sidebar.classList.contains('active')) localStorage.setItem('open', false);
+  sidebar.classList.toggle("active");
+  if (sidebar.classList.contains("active")) localStorage.setItem("open", true);
+  if (!sidebar.classList.contains("active")) localStorage.setItem("open", false);
 });
